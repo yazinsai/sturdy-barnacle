b101d335f50fcc6c60f352489ecc4a1b7a61d8bd
Yazin <github-7itbcg5@yaz.in>
2021-09-02 12:27:46 +0300

    Log userId

--- functions/src/middlewares/auth.ts
+++ functions/src/middlewares/auth.ts
@@ -1,31 +1,31 @@
 import * as admin from "firebase-admin"
 import * as express from 'express';
 
+// Good reference: https://dev.to/emeka/securing-your-express-node-js-api-with-firebase-auth-4b5f
+
 const validateWithToken = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-  console.log("validateWithToken()")
-  const fail = (msg: string) => 
-    res.status(401).send(JSON.stringify({ error: msg }))
+  const error = (msg: string) => res.status(401).send({ error: msg })
 
   // Extract the token
   const authHeader: string = req.get('authorization') ?? ''
-  if(authHeader.length == 0) return fail('Authorization header missing')
+  if(authHeader.length == 0) return error('Authorization header missing')
   
   let [scheme, token] = authHeader.split(' ')
-  if(scheme != "Bearer" || token.length == 0) return fail('Malformed Authorization header')
+  if(scheme != "Bearer" || token.length == 0) return error('Malformed Authorization header')
 
   // Validate the token
   try {
-    const decoded:string = await verifyToken(token)
-    res.locals.auth = JSON.parse(decoded)
+    const decoded = await verifyToken(token)
+    res.locals.userId = decoded.uid
+    res.locals.email = decoded.email
     res.locals.authenticated = true
+    return next()
   } catch(e) {
-    return fail(`Unable to decode the provided token: "${token}"`)
+    return error(`Unable to decode the provided token: "${token}"`)
   }
-
-  return next()
 }
 
-const verifyToken = async (token: string) => {
+const verifyToken = async (token: string): Promise<admin.auth.DecodedIdToken> => {
   try {
     const {decodedToken} = await admin.auth().verifyIdToken(token)
     return decodedToken
