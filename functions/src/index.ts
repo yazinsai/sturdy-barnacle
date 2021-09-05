6bf89071e4cbccf7466437cffd1cde62716dbd9c
Yazin <github-7itbcg5@yaz.in>
2021-09-05 08:38:52 +0300

    Added DB connection

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,16 +2,22 @@ import * as functions from "firebase-functions";
 import express from "express";
 import { addEntry, getEntries } from "./controllers/entry";
 import { validateWithToken } from "./middlewares/auth";
-import {connectDb} from "./models";
+import { connectDb } from "./models";
 
 require('dotenv').config()
 
 const app = express()
 app.use(validateWithToken) // require authentication on all routes
-app.get('/', (req, res) => res.status(200).send('Hi!'))
+app.get('/', (req, res) => res.status(200).send({ 
+  message: "You've successfully authenticated with the Amal API"
+}))
+
 app.post('/entries', addEntry)
 app.get('/entries', getEntries)
 
-connectDb().then(async () => {
-  exports.app = functions.https.onRequest(app)
-})
+const onLaunch = async () => {
+  await connectDb()
+}
+
+exports.app = functions.https.onRequest(app)
+onLaunch()
