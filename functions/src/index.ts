473701a38fd1321fb26681e3d08f911117283cf6
Yazin <github-7itbcg5@yaz.in>
2021-09-21 14:15:37 +0300

    Minor

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,29 +9,24 @@ import { isProduction } from "./config/constants";
 import db from "./database";
 import Cabin from 'cabin';
 
-import { login } from "./lib/alpaca"
 import { getUsers } from "./controllers/users";
 import { getPortfolioById, getPortfolios } from "./controllers/portfolios";
 
 console.info(`Running in ${isProduction ? 'production': 'dev'} environment`)
 
 const app = express()
-
-// Setup logging
 const cabin = new Cabin();
-app.use(cabin.middleware);
 
-// Routes
+// Middleware
+app.use(cabin.middleware); // logging
 app.use(validateWithToken) // on all routes
 app.use(paginate.middleware(20, 100)) // default page size is 20; max 100
+
+// Routes
 app.get('/', async (req, res) => res.status(200).send({ message: "👋" }))
 app.get('/users', getUsers)
 app.get('/portfolios', getPortfolios)
 app.get('/portfolios/:id', getPortfolioById)
-app.get('/accounts', async (req, res) => {
-  const accounts = await login()
-  return res.status(200).send({ accounts: accounts })
-})
 
 // Setup
 const setup = async () => {
